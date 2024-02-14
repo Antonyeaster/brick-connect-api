399a40883bc64a2cac1ee849121d3321cd3ffc4d
Antony Easter <antonyeaster@live.co.uk>
2024-02-14 11:17:55 +0000

    Create comment like app and add model

---
+++ comment_like/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+from comments.models import Comment
+
+
+class CommentLike(models.Model):
+    """
+    CommentLike model represents a 'like' on a comment.
+    It relates to the User and Comment model.
+    'owner' relating to the User modal for the 
+    user that liked the comment.
+    'comment' relating to the comment model for the
+    comment that has been liked.
+    Using 'unique_together' ensures the same user cant like the
+    same comment twice.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, related_name='likes', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'comment']
+
+    def __str__(self):
+        return f'{self.owner} {self.comment}' 
