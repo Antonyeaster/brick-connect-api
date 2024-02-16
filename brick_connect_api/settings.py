07b1cf6ad2a560a01f09bdfe24c54428275cd816
Antony Easter <antonyeaster@live.co.uk>
2024-02-16 11:55:45 +0000

    Update heroku allowed host link

--- brick_connect_api/settings.py
+++ brick_connect_api/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'brick_connect_api.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'brick-connect-api-e6b45c41a17a.herokuapp.com']
 
 
 # Application definition
