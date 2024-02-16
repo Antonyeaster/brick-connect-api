dd8ace6b7e0cd7ce5dc973d2935b78db28391895
Antony Easter <antonyeaster@live.co.uk>
2024-02-16 11:59:38 +0000

    Change debug to true for heroku deployment

--- brick_connect_api/settings.py
+++ brick_connect_api/settings.py
@@ -59,7 +59,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'brick-connect-api-e6b45c41a17a.herokuapp.com']
 
