20921425a1a467d5c88b583b017a4138272379b3
Antony Easter <antonyeaster@live.co.uk>
2024-03-08 08:03:58 +0000

    Addjust comments views and serializer to handle commentlike

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Comment
+from comment_like.models import CommentLike
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -7,16 +8,27 @@ class CommentSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    commentlike_id = serializers.SerializerMethodField()
+    commentlike_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_commentlike_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            commentlike = CommentLike.objects.filter(
+                owner=user, comment=obj
+            ).first()
+            return commentlike.id if commentlike else None
+        return None
+
     class Meta:
         model = Comment
         fields = [
             'id', 'owner', 'created_at', 'updated_at',
-            'post', 'is_owner', 'profile_id', 'profile_image', 'content',
+            'post', 'is_owner', 'profile_id', 'profile_image', 'content', 'commentlike_id', 'commentlike_count',
         ]
 
 
--- comments/views.py
+++ comments/views.py
@@ -1,5 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics, permissions
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
 from brick_connect_api.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
@@ -8,10 +9,25 @@ from .serializers import CommentSerializer, CommentDetailSerializer
 class CommentList(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Comment.objects.all()
+    queryset = Comment.objects.annotate(
+        commentlike_count=Count('commentlike', distinct=True)
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
 
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['post']
+    filterset_fields = [
+        'post',
+        'commentlike__owner__profile'
+    ]
+
+    ordering_fields = [
+        'commentlike_count',
+        'commentlike__created_at',
+    ]
 
 
     def perform_create(self, serializer):
@@ -21,4 +37,6 @@ class CommentList(generics.ListCreateAPIView):
 class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = CommentDetailSerializer
-    queryset = Comment.objects.all()
+    queryset = Comment.objects.annotate(
+        commentlikes_count=Count('commentlike', distinct=True)
+    ).order_by('-created_at')
