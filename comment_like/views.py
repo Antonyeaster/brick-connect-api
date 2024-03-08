02f54ebde92b804586e6de62da620a6de454be5a
Antony Easter <antonyeaster@live.co.uk>
2024-03-08 08:02:11 +0000

    Add commentlike views

--- comment_like/views.py
+++ comment_like/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from brick_connect_api.permissions import IsOwnerOrReadOnly
+from .models import CommentLike
+from .serializers import CommentLikeSerializer
 
-# Create your views here.
+
+class CommentLikeList(generics.ListCreateAPIView):
+    serializer_class = CommentLikeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = CommentLike.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CommentLikeDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = CommentLikeSerializer
+    queryset = CommentLike.objects.all()
