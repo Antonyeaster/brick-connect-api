25c1dd5ad322a16949079fccc4af5e929ed2d4c0
Antony Easter <antonyeaster@live.co.uk>
2024-03-12 08:23:43 +0000

    Add fields to comment serializer

--- comments/serializers.py
+++ comments/serializers.py
@@ -11,6 +11,8 @@ class CommentSerializer(serializers.ModelSerializer):
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     commentlike_id = serializers.SerializerMethodField()
     commentlike_count = serializers.ReadOnlyField()
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
