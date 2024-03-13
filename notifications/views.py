ae90fa5f4895205ced04e66ed2a28358ff833177
Antony Easter <antonyeaster@live.co.uk>
2024-03-13 08:26:43 +0000

    Add notifications lis and detail view

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics, permissions, filters
+from .models import Notifications
+from .serializers import NotificationsSerializer
 
-# Create your views here.
+
+class NotificationsList(generics.ListAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = NotificationsSerializer
+
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ["read", "created_at"]
+
+    def get_queryset(self):
+        return Notifications.objects.filter(recipient=self.request.user, read=False)
+
+
+class NotificationsDetail(generics.RetrieveUpdateAPIView):
+    queryset = Notifications.objects.all()
+    serializer_class = NotificationsSerializer
+    permission_classes = [permissions.IsAuthenticated]
