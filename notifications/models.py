d6895bd8deb7267409cfb77da53c443e14653780
Antony Easter <antonyeaster@live.co.uk>
2024-03-13 08:25:22 +0000

    Add categories for follow and comment notifications

--- notifications/models.py
+++ notifications/models.py
@@ -3,11 +3,21 @@ from django.contrib.auth.models import User
 
 
 class Notifications(models.Model):
+
+    FOLLOW = 'follow'
+    COMMENT = 'comment'
+
+    CATEGORY_CHOICES = [
+        (FOLLOW, 'Follow'),
+        (COMMENT, 'Comment'),
+    ]
+
     recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name="received_notifications")
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_notifications")
     text = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     read = models.BooleanField(default=False)
+    category = models.CharField(max_length=40, choices=CATEGORY_CHOICES, null=True)
 
     class Meta:
         ordering = ['-created_at']
