66a455a132bcb2824ebd65e0a460a384c6a48c7f
Antony Easter <antonyeaster@live.co.uk>
2024-03-15 22:14:47 +0000

    Add notification views

--- notifications/views.py
+++ notifications/views.py
@@ -2,31 +2,28 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics, permissions, filters
 from .models import Notifications
 from .serializers import NotificationsSerializer
+from brick_connect_api.permissions import IsNotificationsrecipient
 
 
-class NotificationsList(generics.ListCreateAPIView):
-    permission_classes = [permissions.IsAuthenticated]
+class NotificationsList(generics.ListAPIView):
+    
     serializer_class = NotificationsSerializer
 
+    permission_classes = [permissions.IsAuthenticated]
+
     filter_backends = [filters.OrderingFilter]
-    ordering_fields = ["read", "created_at"]
+    ordering_fields = ["created_at", "read"]
+    
 
     def get_queryset(self):
         return Notifications.objects.filter(recipient=self.request.user)
 
     def perform_create(self, serializer):
+        serializer.save(recipient=self.request.user)
 
-        read_value = self.request.data.get('read', '').lower() == 'true'
-        
-        serializer.save(
-            recipient=self.request.user,
-            sender=self.request.user,
-            read=read_value,
-            category=self.request.data.get('category', ''),
-            object_id=self.request.data.get('object_id', None),
-        )
 
 class NotificationsDetail(generics.RetrieveUpdateDestroyAPIView):
+
     queryset = Notifications.objects.all()
     serializer_class = NotificationsSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsNotificationsrecipient]
