54d52a38eefd69cf6442032f77a0330fe159dbf5
Antony Easter <antonyeaster@live.co.uk>
2024-03-15 22:12:45 +0000

    Add permission for notification recipient

--- brick_connect_api/permissions.py
+++ brick_connect_api/permissions.py
@@ -1,5 +1,14 @@
 from rest_framework import permissions
+from rest_framework.exceptions import PermissionDenied
 
+class IsNotificationsrecipient(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if obj.recipient == request.user:
+            return True
+        raise PermissionDenied(
+            "403 Permission has been denied"
+        )
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
