bac8fb9d77d55c52d10940436e22cfab65f503f1
Antony Easter <antonyeaster@live.co.uk>
2024-03-17 18:27:32 +0000

    Remove ready method for test purpose

--- notifications/apps.py
+++ notifications/apps.py
@@ -4,14 +4,3 @@ from django.apps import AppConfig
 class NotificationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'notifications'
-
-    def ready(self):
-        from .signals import create_follow_notification, create_comment_notification
-        
-        from django.db.models.signals import post_save
-        from followers.models import Follower  
-        from comments.models import Comment 
-        
-        post_save.connect(create_follow_notification, sender=Follower)
-        
-        post_save.connect(create_comment_notification, sender=Comment)
